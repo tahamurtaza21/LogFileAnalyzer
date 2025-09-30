32635219a9b18b7957fcd171472f5e33398dac7f
Taha Murtaza Ali <tahamurtaza21@outlook.com>
2025-09-30 08:19:25 +0100

    Reading file paths

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -1,17 +1,38 @@
 package Application;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+
+        ConcurrentHashMap<String, Integer> sharedMap = new ConcurrentHashMap<String,Integer>();
+
+        Path path1 = Path.of(Objects.requireNonNull(Main.class.getResource("/server1.log")).toURI());
+        Path path2 = Path.of(Objects.requireNonNull(Main.class.getResource("/server2.log")).toURI());
+
+        List<String> allContent1 = Files.readAllLines(path1, StandardCharsets.UTF_8);
+        List<String> allContent2 = Files.readAllLines(path2, StandardCharsets.UTF_8);
+
+        System.out.println(CountLogWords(allContent1, sharedMap));
+    }
+
+    private static ConcurrentHashMap<String, Integer> CountLogWords(List<String> allContent1, ConcurrentHashMap<String, Integer> sharedMap) {
+        for (String line : allContent1)
+        {
+            String[] words = line.split("\\W+");
+            for (String word : words) {
+                if (word.equals("WARNING") || word.equals("SEVERE") || word.equals("LOG")) {
+                    sharedMap.put(word, sharedMap.getOrDefault(word, 0) + 1);
+                }
+            }
         }
+        return sharedMap;
     }
 }
